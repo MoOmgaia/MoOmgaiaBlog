5849cf0b4717cfee4fb2da71f1185c5fc31ffd38
moomgaia <moomgai@free.fr>
2025-04-21 20:42:17 +0200

    Add Observable Runtime

---
+++ src/components/post-components/monde_creux/coupe.jsx
@@ -0,0 +1,31 @@
+import { useState, useRef } from 'react'
+import { LoadingCircle } from '@/components/ui/loading-circle'
+import { countries } from './countries'
+
+import React, {useRef, useEffect} from "react";
+import {Runtime, Inspector} from "@observablehq/runtime";
+import notebook from "@linard-y/paramatric-function";
+
+function ParamatricFunction() {
+  const chartRef = useRef();
+  const viewofTimeRef = useRef();
+
+  useEffect(() => {
+    const runtime = new Runtime();
+    runtime.module(notebook, name => {
+      if (name === "chart") return new Inspector(chartRef.current);
+      if (name === "viewof time") return new Inspector(viewofTimeRef.current);
+    });
+    return () => runtime.dispose();
+  }, []);
+
+  return (
+    <>
+      <div ref={chartRef} />
+      <div ref={viewofTimeRef} />
+      <p>Credit: <a href="https://observablehq.com/@linard-y/paramatric-function@1066">Paramatric function by linard-y</a></p>
+    </>
+  );
+}
+
+export default ParamatricFunction;
